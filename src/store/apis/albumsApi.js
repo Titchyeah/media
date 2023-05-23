2871ad3dee20a119983119c28e978da1e8546d6e
Thomas Skelton <tomskelton@live.co.uk>
2023-05-23 20:32:28 +0100

    added addAlbumsMutation for adding new albums

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,12 +1,25 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { faker } from '@faker-js/faker';
 
 const albumsApi = createApi({
     reducerPath: 'albums',
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:3005'
     }),
-    endpoints (builder) {
+    endpoints(builder) {
         return {
+            addAlbum: builder.mutation({
+                query: (user) => {
+                    return {
+                        url: '/albums',
+                        method: 'POST',
+                        body: {
+                            userId: user.id,
+                            title: faker.commerce.productName(),
+                        },
+                    };
+                }
+            }),
             fetchAlbums: builder.query({
                 query: (user) => {
                     return {
@@ -28,5 +41,8 @@ const albumsApi = createApi({
     }
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { 
+    useFetchAlbumsQuery,
+    useAddAlbumMutation
+  } = albumsApi;
 export { albumsApi };
