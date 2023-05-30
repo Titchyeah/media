bb6bf5f386708e1f178fea552fc3686efef34df5
Thomas Skelton <tomskelton@live.co.uk>
2023-05-30 22:27:09 +0100

    added photos component and finished prject

--- src/components/AlbumsListItem.js
+++ src/components/AlbumsListItem.js
@@ -2,6 +2,7 @@ import Button from "./Button";
 import ExpandablePanel from "./ExpandablePanel";
 import { GoTrashcan } from "react-icons/go";
 import { useRemoveAlbumMutation } from "../store";
+import PhotosList from "./PhotosList";
 
 function AlbumsListItem({ album }) {
 
@@ -11,18 +12,18 @@ function AlbumsListItem({ album }) {
         removeAlbum(album)
     }
     const header = (
-    <>
-        <Button className="mr-2" loading={results.isLoading} onClick={handleRemoveAlbum}>
-            <GoTrashcan />
-        </Button>
-        {album.title}
-    </>
+        <>
+            <Button className="mr-2" loading={results.isLoading} onClick={handleRemoveAlbum}>
+                <GoTrashcan />
+            </Button>
+            {album.title}
+        </>
     )
 
 
     return (
         <ExpandablePanel key={album.id} header={header}>
-            List of photos in the album
+            <PhotosList album={album} />
         </ExpandablePanel>
     );
 }
